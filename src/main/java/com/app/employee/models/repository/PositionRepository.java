ef79c2e162fd7f5226a05dc8b5c24566d658ead0
wrosorio <wrosorio@indracompany.com>
2021-06-27 09:07:16 -0500

    Add findbyName position model

--- src/main/java/com/app/employee/models/repository/PositionRepository.java
+++ src/main/java/com/app/employee/models/repository/PositionRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.app.employee.models.entity.Position;
 
 public interface PositionRepository extends JpaRepository<Position, Long>{
+	
+	Position findByName(String name);
 
 }
