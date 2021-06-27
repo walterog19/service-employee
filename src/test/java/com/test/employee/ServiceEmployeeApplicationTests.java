8f0a313d7c25793ff103c87e38ac25cad848a718
wrosorio <wrosorio@indracompany.com>
2021-06-27 18:46:40 -0500

    Add Junit

--- src/test/java/com/test/employee/ServiceEmployeeApplicationTests.java
+++ src/test/java/com/test/employee/ServiceEmployeeApplicationTests.java
@@ -1,13 +1,26 @@
 package com.test.employee;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(value = "/application.properties")
 class ServiceEmployeeApplicationTests {
+	@Test
+	public void contextLoads() {
+		// este método usa el properties indicado en TestPropertySource y así se prueba
+		// el caso de CORS desactivado.
+	}
 
 	@Test
-	void contextLoads() {
+	public void main() {
+		// este método NO usa el properties indicado en TestPropertySource y así se prueba
+		// el caso de CORS activado.
+		com.app.employee.ServiceEmployeeApplication.main(new String[] {});
 	}
 
 }
